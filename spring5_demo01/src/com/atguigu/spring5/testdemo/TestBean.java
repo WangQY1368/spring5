9e1bec6f50d41a2eb231d20e12edffcb5924f767
WangQY1368 <289924569@qq.com>
2022-08-26 18:31:50 +0800

    完成demo01

---
+++ spring5_demo01/src/com/atguigu/spring5/testdemo/TestBean.java
@@ -0,0 +1,43 @@
+package com.atguigu.spring5.testdemo;
+
+import com.atguigu.spring5.Orders;
+import com.atguigu.spring5.bean.Emp;
+import com.atguigu.spring5.service.UserService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestBean {
+    @Test
+    public void testAdd() {
+        //1、加载spring配置文件
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
+
+        //2、获取配置创建的对象
+        UserService userService = context.getBean("userService", UserService.class);
+
+        userService.add();
+    }
+
+    @Test
+    public void testBean1() {
+        //1、加载spring配置文件
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean3.xml");
+
+        //2、获取配置创建的对象
+        Emp emp = context.getBean("emp", Emp.class);
+
+        emp.add();
+    }
+
+    @Test
+    public void testBean2() {
+        //1、加载spring配置文件
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean4.xml");
+
+        //2、获取配置创建的对象
+        Emp emp = context.getBean("emp", Emp.class);
+
+        emp.add();
+    }
+}
